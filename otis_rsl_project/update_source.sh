0283af3bbfef8887e6819f403618bd2998da62e0
Dmitry Akimov <von.akimov@gmail.com>
2023-06-29 15:47:08 +0300

    # ADD update_source.sh

---
+++ otis_rsl_project/update_source.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Start update code routine!"
+fetch=$(sudo git fetch)
+
+if [[ -z "$fetch" ]] 
+then 
+    echo "Local Repo up to date, no git pull needed" && exit 1 
+else 
+    sudo git pull && echo "Make source code" ; make  || echo "git pull failed, exiting." ; exit 1 
+fi
