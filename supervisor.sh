e8aac3b74fdb85202edfeba90e3a5237b28a8a7d
Dmitry Akimov <von.akimov@gmail.com>
2023-07-11 10:51:37 +0300

    # WORKING ON SCRIPT

--- supervisor.sh
+++ supervisor.sh
@@ -7,80 +7,8 @@ EXEC_PATH=~/OTIS_RSL_DISPLAY/source
 SLEEP_TIME=5
 
 # Work until main process exists (pgrep ret code == 0)
-while  pgrep -x source > /dev/null
-do
-    echo "Just working routine! Check for mounted usb devices!"
+#while  pgrep -x source > /dev/null
 
-    # Check if usb flash drive is present as block device
-    if ls /./dev/sd*
-    then
-        # Check if player is working
-        if pgrep -x omxplayer > /dev/null
-        then
-			 pkill omxplayer
-             echo "Static image mode"
-        else
-            # Find usb drive mountpoint path and cd to it
-            MOUNT_DIR=$(lsblk -o mountpoint | grep 'media')
-            cd $MOUNT_DIR
-            
-            # Try to find video files with estimated names 01.mp4-99.mp4
-            if ls [0-9][1-9].mp4
-            then
-                echo "Found some videos!"
-                
-                # Check if output.mp4 already exists and delete if it is
-                cd $EXEC_PATH
-                [ -e video/output.mp4 ] && rm video/output.mp4
-                
-                # Merge videos into one file
-                cd $MOUNT_DIR
-                
-                 # Delete temp files
-                rm *.ts
-                
-                for video in [0-9][1-9].mp4; do
-                    ffmpeg -i $video -c copy -bsf:v h264_mp4toannexb -f mpegts $video.ts
-                done
-
-                CONCAT=$(echo $(ls -v *.ts) | sed -e "s/ /|/g")
-		
-                ffmpeg -i "concat:$CONCAT" -c copy -bsf:a aac_adtstoasc /home/pi/Desktop/otis_project/pngview/video/output.mp4
-
-                # Delete temp files
-                rm *.ts
-               
-                echo "Dynamic image mode" 
-				
-                cd $EXEC_PATH
-                #pkill rasp_otis
-                #omxplayer  --loop --no-osd video/output.mp4 &
-                #./rasp_otis -dynamic /dev/ttyAMA0  &
-				# Restart supervisor script
-                #exec ./supervisor.sh
-            else
-                echo "Video files aren't present. Static image mode"    
-            fi
-        fi    
-    else 
-        if pgrep -x omxplayer > /dev/null
-        then
-            echo "Static image mode"
-            #cd $EXEC_PATH
-            #pkill omxplayer
-            #pkill rasp_otis
-            #./rasp_otis -static  /dev/ttyAMA0 &
-			# Restart supervisor script
-            #exec ./supervisor.sh
-        fi
-        
-    fi
-
-    # Sleep for TIMEOUT seconds
-    echo "Sleep for $SLEEP_TIME seconds"
-    sleep $SLEEP_TIME
-    
-done
 
 
 
@@ -97,19 +25,21 @@ do
         then
 			 pkill omxplayer
              echo "Static image mode"
-        else
-            # Find usb drive mountpoint path and cd to it
-            MOUNT_DIR=$(lsblk -o mountpoint | grep 'media')
-            cd $MOUNT_DIR
+			 # Start STATIC MODE OF MAIN PROCESS
+			 sleep $SLEEP_TIME
+        
+        # Find usb drive mountpoint path and cd to it
+        MOUNT_DIR=$(lsblk -o mountpoint | grep 'media')
+        cd $MOUNT_DIR
             
-            # Try to find video files with estimated names 01.mp4-99.mp4
-            if ls [0-9][1-9].mp4
+        # Try to find video files with estimated names 01.mp4-99.mp4
+        if ls [0-9][1-9].mp4
             then
                 echo "Found some videos!"
                 
                 # Check if output.mp4 already exists and delete if it is
                 cd $EXEC_PATH
-                [ -e video/output.mp4 ] && rm video/output.mp4
+                [ -e videos/output.mp4 ] && rm videos/output.mp4
                 
                 # Merge videos into one file
                 cd $MOUNT_DIR
@@ -123,7 +53,7 @@ do
 
                 CONCAT=$(echo $(ls -v *.ts) | sed -e "s/ /|/g")
 		
-                ffmpeg -i "concat:$CONCAT" -c copy -bsf:a aac_adtstoasc /home/pi/Desktop/otis_project/pngview/video/output.mp4
+                ffmpeg -i "concat:$CONCAT" -c copy -bsf:a aac_adtstoasc ~/OTIS_RSL_DISPLAY/source/videos/output.mp4
 
                 # Delete temp files
                 rm *.ts
@@ -139,7 +69,7 @@ do
             else
                 echo "Video files aren't present. Static image mode"    
             fi
-        fi    
+           
     else 
 		echo "There is no usb devices present!"
         if pgrep -x omxplayer > /dev/null
